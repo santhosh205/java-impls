9fce62abbb40ce3f64fba312e515d31182f2c17a
santhosh205 <santhoshdasari205@gmail.com>
2021-07-18 21:15:42 +0530

    Refactor(impl-one): separated out constants

--- one/src/main/java/javaimpls/one/controller/ImplOneController.java
+++ one/src/main/java/javaimpls/one/controller/ImplOneController.java
@@ -4,14 +4,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static javaimpls.one.utils.Constants.*;
 import static org.springframework.http.MediaType.TEXT_PLAIN_VALUE;
 
 @RestController
-@RequestMapping("/api/v1/one")
+@RequestMapping(BASE_REQUEST_MAPPING)
 public class ImplOneController {
 
-    @GetMapping(path = "/", produces = TEXT_PLAIN_VALUE)
+    @GetMapping(path = JUST_SLASH, produces = TEXT_PLAIN_VALUE)
     public String getPrintText() {
-        return "This is 1st implementation.";
+        return IMPL_ONE_STATEMENT;
     }
 }
