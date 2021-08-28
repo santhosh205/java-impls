7181e82b8003f64309f6601950d7d0250895e07a
santhosh205 <santhoshdasari205@gmail.com>
2021-08-28 12:21:46 +0530

    fix(app-run): moved application to base package

--- one/src/main/java/javaimpls/one/controller/ImplOneController.java
+++ one/src/main/java/javaimpls/one/controller/ImplOneController.java
@@ -1,18 +1,19 @@
 package javaimpls.one.controller;
 
+import javaimpls.one.models.Greeting;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import static javaimpls.one.utils.Constants.*;
-import static org.springframework.http.MediaType.TEXT_PLAIN_VALUE;
+import static javaimpls.one.utils.Constants.BASE_REQUEST_MAPPING;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequestMapping(BASE_REQUEST_MAPPING)
 public class ImplOneController {
 
-    @GetMapping(path = JUST_SLASH, produces = TEXT_PLAIN_VALUE)
-    public String getPrintText() {
-        return IMPL_ONE_STATEMENT;
+    @GetMapping(path = "", produces = APPLICATION_JSON_VALUE)
+    public Greeting getGreeting() {
+        return new Greeting(1L, "Hello World!");
     }
 }
--- one/src/main/java/javaimpls/one/utils/Constants.java
+++ one/src/main/java/javaimpls/one/utils/Constants.java
@@ -5,6 +5,4 @@ public class Constants {
     private Constants() {}
 
     public static final String BASE_REQUEST_MAPPING = "/api/v1/one";
-    public static final String JUST_SLASH = "/";
-    public static final String IMPL_ONE_STATEMENT = "This is 1st implementation.";
 }
